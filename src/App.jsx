f96960350703c253178abcf9d1a55122d51a91d8
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-11-07 15:00:21 -0600

    Init Search layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import Header from './components/layouts/Header/Header'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Search from './components/layouts/Search/Search';
 
 function App() {
   return (
     <div id='App' className="App">
       <Header />
+      <Search />
     </div>
   )
 }
