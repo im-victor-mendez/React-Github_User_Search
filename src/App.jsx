74efde0c3ca0ce63e67ca5f5a80e8dd098bd4b42
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-11-08 20:21:09 -0600

    Implemented Profile layout.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.scss'
 import Header from './components/layouts/Header/Header'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Search from './components/layouts/Search/Search';
+import Profile from './components/layouts/Profile/Profile';
 
 function App() {
   return (
     <div id='App' className="App">
       <Header />
       <Search />
+      <Profile />
     </div>
   )
 }
