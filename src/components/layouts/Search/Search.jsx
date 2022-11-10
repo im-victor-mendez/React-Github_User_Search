83b21f7d961b2ea8bece2fe7e6f4111ee8b28a78
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-11-09 21:19:24 -0600

    Code factorization of Search component.

--- src/components/layouts/Search/Search.jsx
+++ src/components/layouts/Search/Search.jsx
@@ -4,10 +4,22 @@ import { Button } from 'react-bootstrap'
 import { getGitHubUser } from "../../../utils/gitHubAPI";
 import { useDispatch } from "react-redux";
 import { setUser } from '../../../stateManagement/userSlice';
+import { searchIcon } from '../../../media/icons/SearchIcon';
 
 function Search() {
     const dispatch = useDispatch()
     const [input, setInput] = useState()
+
+    function setUserStore() {
+        getGitHubUser(input)
+        .then(
+            data => {
+                dispatch(
+                    setUser(data)
+                )
+            }
+        )
+    }
 
     return (
             <form
@@ -15,15 +27,8 @@ function Search() {
             onSubmit={
                 (event) => {
                     event.preventDefault()
-                    console.log(input)
-                    getGitHubUser(input)
-                    .then(
-                        data => {
-                            dispatch(
-                                setUser(data)
-                            )
-                        }
-                    )
+
+                    setUserStore()
                 }
             }
             >
@@ -33,10 +38,7 @@ function Search() {
                 onChange={event => setInput(event.target.value)} />
 
                 <div id='input-inside'>
-                    {/* Fill = --bs-primary: #0d6efd; */}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#0d6efd" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                    </svg>
+                    {searchIcon}
                     <Button type='submit'>Search</Button>
                 </div>
             </form>
